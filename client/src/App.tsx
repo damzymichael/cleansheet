5fe86384b801ac1f5fba0deea19ab37922acb8c3
damzymike <damzymike@gmail.com>
2026-09-05 15:51:09 +0100

    protected router code fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -40,10 +40,10 @@ function ProtectedRoute({ children }: { children: React.ReactNode }) {
     const isAuthenticated = useAuthStore(state => state.isAuthenticated);
     const location = useLocation();
 
-    // if (!isAuthenticated) {
-    //     // Redirect them to the /login page, but save the current location they were trying to go to
-    //     return <Navigate to="/login" state={{ from: location }} replace />;
-    // }
+    if (!isAuthenticated) {
+        // Redirect them to the /login page, but save the current location they were trying to go to
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
 
     return <>{children}</>;
 }
